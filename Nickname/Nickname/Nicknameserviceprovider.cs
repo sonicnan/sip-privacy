3eaabba550328f7491f3e49960c74e17776ba405
sonicnan <sonicnan@hotmail.com>
2014-03-17 22:01:05 +0700

    Username to Nickname

---
+++ Nickname/Nickname/Nicknameserviceprovider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Security;
+
+namespace Nickname
+{
+    public class Nicknameserviceprovider
+    {
+        List<Nickname> NameList = new List<Nickname>();
+
+        public string getNickname(string inputname)
+        {
+            string name = RNGRandom.RandomString(8);
+            NameList.Add(new Nickname() { username = inputname, nickname=name });
+            return name;
+        }
+
+        public string getUsername(string inputname)
+        {
+            string outputname = "";
+            try
+            {
+                Nickname agedTwenty = NameList.Where<Nickname>(x => x.nickname == inputname).Single<Nickname>();
+                int index = NameList.IndexOf(agedTwenty);
+                outputname = NameList[index].username;
+                NameList.RemoveAt(index);
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Cannot Found\n");
+            }
+
+            return outputname;
+        }
+
+    }
+}
